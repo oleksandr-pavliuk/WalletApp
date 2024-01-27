c5ed1654c449a88d9396d91a65106465ab0be2ff
oleksandr-pavliuk <61949378+FoldeR-ua@users.noreply.github.com>
2024-01-27 19:15:39 +0200

    WalletApp: DataSeeder for testing

---
+++ WalletApp/Application/Helpers/DataSeeder.cs
@@ -0,0 +1,34 @@
+﻿
+using WalletApp.Domain;
+using WalletApp.Infrastructure.DatabaseContext;
+
+namespace WalletApp.Application.Helpers
+{
+    public static class DataSeeder
+    {
+
+
+        public static void Seed()
+        {
+            using (var _context = new ApplicationContext())
+            {
+                if (_context.Users.Count() == 0 && _context.Transactions.Count() == 0)
+                {
+                    var user = new User() { Points = 1043, Balance = 1000 };
+
+                    var transactionList = new List<Transaction>()
+                {
+                    new Transaction() { User = user, Bank = "monobank", Date = DateTime.Now, Description = "Purchases", Name = "Netflix",
+                                        Sum = -120.00M, TransactionStatus = Status.Accepted },
+                    new Transaction() { User = user, Bank = "wallet", Date = DateTime.Now, Description = "Income", Name = "Apple",
+                                        Sum = 100.00M, TransactionStatus = Status.Pending, UserSender = "Alex"}
+                };
+
+                    _context.Users.Add(user);
+                    _context.Transactions.AddRange(transactionList);
+                    _context.SaveChanges();
+                }
+            }   
+        }
+    }
+}
